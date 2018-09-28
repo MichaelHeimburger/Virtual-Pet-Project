cdb5e71fb5a763db52246da0bde5410418960c85
MichaelHeimburger <mrheimburger45@gmail.com>
2018-09-28 18:34:42 -0400

    Started a few lines of threading code need to think how to implement to self-teach

--- Virtual Pet/Program.cs
+++ Virtual Pet/Program.cs
@@ -3,36 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Virtual_Pet
 {
     class Program
     {
-        static void Main(string[] args)
+        Pet doggo = new Pet("defualt_name");
+
+
+        static void Main()
         {
             string userInput;
-            DogArt(); // Beginning Screen
             Console.WriteLine("Congratulations you are a recipiant of the new Virtual Pet. What are you going to name it?");
-            Pet doggo = new Pet(Console.ReadLine());
-
-
+            Program program = new Program();
+            new Thread(program.DogArt).Start();
             while (5 > 1) // infinitely loops the game
-            {
-                Console.Clear();
-                //Pet Status
-                Console.WriteLine("{0} the Virtual Pet's current status levels", doggo.Name);
-                Console.WriteLine("Hunger: {0}", doggo.Hunger);
-                Console.WriteLine("Thirst: {0}", doggo.Thirst);
-                Console.WriteLine("Boredom: {0}", doggo.Boredom);
-
-                DogArt();
-                Console.WriteLine("--------------------------------------------------------------------------------");
-                //Pet Inputs
-                Console.WriteLine("What would you like to do today with {0}", doggo.Name);
-                Console.WriteLine("Play?");
-                Console.WriteLine("Feed?");
-                Console.WriteLine("Water?");
-                Console.WriteLine("Please type response below.");
+            { 
                 while (5 > 1) // Infinite loop that will only exit upon correct user input
                 {
                     userInput = Console.ReadLine().ToLower();
@@ -59,13 +46,28 @@ namespace Virtual_Pet
 
         }
 
-        static void DogArt() // method shortcut for the ascii dog art - makes the code look nicer imo if its down here
+        void DogArt() // method shortcut for the ascii dog art - makes the code look nicer imo if its down here
         {
+
+            Console.Clear();
+            //Pet Status
+            Console.WriteLine("{0} the Virtual Pet's current status levels", doggo.Name);
+            Console.WriteLine("Hunger: {0}", doggo.Hunger);
+            Console.WriteLine("Thirst: {0}", doggo.Thirst);
+            Console.WriteLine("Boredom: {0}", doggo.Boredom);
+            //DogArt
             Console.WriteLine("   __");
             Console.WriteLine("o-''|\\_____/)");
             Console.WriteLine(" \\_/|_)     )");
             Console.WriteLine("    \\  __  /");
             Console.WriteLine("    (_/ (_/ ");
+            Console.WriteLine("--------------------------------------------------------------------------------");
+            //Pet Inputs
+            Console.WriteLine("What would you like to do today with {0}", doggo.Name);
+            Console.WriteLine("Play?");
+            Console.WriteLine("Feed?");
+            Console.WriteLine("Water?");
+            Console.WriteLine("Please type response below.");
         }
 
 
